f49bcd61600cf01f03bdbb5271d4cb71b4266817
HatteSoul <akira.hattesohl@innere.med.uni-giessen.de>
2019-08-12 08:43:59 +0200

    added newline to output

--- plot_feature_distribution.R
+++ plot_feature_distribution.R
@@ -139,7 +139,7 @@ cat("summary:\n  barcodes: ", attr(mat,'Dim')[2], "\n  features: ", attr(mat,'Di
 cat("removing features that are not expressed at all ...")
 reducedMat = mat[apply(mat, 1, function(row) any(row !=0 )), ]
 cat(" done.\n")
-cat("summary:\n  removed features: ", dim(mat)[1] - dim(reducedMat)[1], "\n  features left: ", dim(reducedMat)[1], sep = "")
+cat("summary:\n  removed features: ", dim(mat)[1] - dim(reducedMat)[1], "\n  features left: ", dim(reducedMat)[1], "\n", sep = "")
 
 # count UMIs
 cat("counting UMIs per barcode ...")
